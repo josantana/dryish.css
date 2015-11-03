93ec51cc36634d4eb962ac5cbce5eb9f37baa360
Jo Santana <josantana@jogalabs.com>
2015-11-03 13:52:11 -0200

    FIX: All files updated with the new repository name.

--- gulpfile.js
+++ gulpfile.js
@@ -42,7 +42,7 @@ gulp.task('styles', function ()
     }))
     .pipe(sourcemaps.write('../map'))
     .pipe(gulp.dest(dist))
-    .pipe(notify('Dryish: Built'));
+    .pipe(notify('upDRY: Built'));
 });
 
 gulp.task('default', ['styles'], function ()
