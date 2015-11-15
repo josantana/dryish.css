6d942a8a32cd9d6a351c7f324fa6da0e7789956e
Jo Santana <josantana@jogalabs.com>
2015-11-14 22:30:50 -0200

    REFACTORING: Layout
    - Now the layout works like a component.
    - Included layout test page.

--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,7 @@ var devInfo =
 ].join('\n');
 
 var dist = './dist',
-    scss = [ './source/*.scss',  './source/includes/*.scss' ];
+    scss = [ './source/*.scss',  './source/includes/*.scss',  './source/selectors/*.scss' ];
 
 gulp.task('styles', function ()
 {
